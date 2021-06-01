f0a719d9c06a197159df03510d41f7060f2b641b
tcseley <tiborcseley@gmail.com>
2021-06-01 15:57:54 -0500

    api call done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
+
 function App() {
+  const [gitUser, setGitUser] = useState([]);
+
+  const returnGitUser = async () => {
+    let url = "https://api.github.com/users/tcseley"
+    const gitUser = await axios.get(url);
+    console.log(gitUser);
+    setGitUser(gitUser.data);
+  };
+  
+  useEffect(() => returnGitUser(), []);
+  
+  
+  
   return (
     <div className="App">
       <h1>Github Clone</h1>
       <img width="350px" height="200px" src="https://img.flaticon.com/icons/png/512/25/25231.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF" alt="" />
+      <div className="dash-repo">
+        <h2 >Repos</h2>
+      </div>
+      <div>
+        <h2>{gitUser && gitUser.length}</h2>
+      </div>
     </div>
   );
 }
--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 
 const Settings = (props) => {
+    const 
+    return(
 
+    )
 }
 
 export default Settings;
